ddcbbffa3f4dc934ebec1e46524697a50b948c20
anshulstArship <anshul.tyagi306@gmail.com>
2024-04-05 16:57:56 +0530

    Adding structure for DMIFinance Application

---
+++ src/main/java/com/main/dmiFinance/DmiFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.main.dmiFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmiFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmiFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/dmiFinance/DmiFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.dmiFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmiFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
